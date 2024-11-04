37796a833cdfb1b76f9210b551eae2a61e44d619
Haj Rezvan <rezvanianragahi2@gmail.com>
2024-11-04 14:17:18 +0000

    ✨: اضافه شدن تاریخ به جای روز، اضافه شدن داشبورد تیم مربیان و احراز هویت 🐛: درست کردن مشکل نبود فیلد در آرایه

--- pages/Login/jsobjects/JSObject1/JSObject1.js
+++ pages/Login/jsobjects/JSObject1/JSObject1.js
@@ -4,10 +4,16 @@ export default {
 		for(const iterator of dataSet){
 			if (iterator.userid===usr){
 				if(iterator.password===pss){
+					if(iterator.roll===4||3) {
 					storeValue("userId",usr)
 					navigateTo('Submit', {}, 'SAME_WINDOW').then(() => {})
-					break;
+					} else if(iterator.roll===2){
+						navigateTo('KomakMorabbi', {}, 'SAME_WINDOW').then(() => {})
+					} else if (iterator.roll===1){
+						navigateTo('Morabbi', {}, 'SAME_WINDOW').then(() => {})
+					}
         }
+					break;
 			}
      }
 	}
