99938f9ab32197dce90fc6a566b932a1dac94cdb
Haj Rezvan <rezvanianragahi2@gmail.com>
2024-11-04 17:08:43 +0000

    ✨: اضافه شدن داشبورد تیم مربیان

--- pages/Login/jsobjects/JSObject1/JSObject1.js
+++ pages/Login/jsobjects/JSObject1/JSObject1.js
@@ -4,14 +4,21 @@ export default {
 		for(const iterator of dataSet){
 			if (iterator.userid===usr){
 				if(iterator.password===pss){
-					if(iterator.roll===4||3) {
+					var key = iterator.roll
 					storeValue("userId",usr)
-					navigateTo('Submit', {}, 'SAME_WINDOW').then(() => {})
-					} else if(iterator.roll===2){
-						navigateTo('KomakMorabbi', {}, 'SAME_WINDOW').then(() => {})
-					} else if (iterator.roll===1){
-						navigateTo('Morabbi', {}, 'SAME_WINDOW').then(() => {})
-					}
+					switch(key){
+           	case 4||3:
+							navigateTo('Submit', {}, 'SAME_WINDOW').then(() => {})
+           		break;
+						case 1:
+							navigateTo('Morabbi', {}, 'SAME_WINDOW').then(() => {})
+							break;
+						case 2:
+							navigateTo('KomakMorabbi', {}, 'SAME_WINDOW').then(() => {})
+							break;
+           	default:
+           		break;
+           }
         }
 					break;
 			}
